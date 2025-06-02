f5298c9620383286464a45997189efa96fe9f9fd
IrinaT <irina.tiora@isa.utm.md>
2025-06-02 23:44:30 +0300

    Generate sqlc queries for session creation and reading

---
+++ db/query/session.sql
@@ -0,0 +1,21 @@
+-- name: GetSessionsByCampaignAndCharacter :many
+SELECT
+    s.*,
+    CASE
+        WHEN sac.character_id IS NOT NULL THEN TRUE
+        ELSE FALSE
+        END AS is_allowed
+FROM "sessions" s
+         LEFT JOIN "session_allowed_characters" sac
+                   ON s.id = sac.session_id AND sac.character_id = $2
+WHERE s.campaign_id = $1;
+
+-- name: CreateSession :one
+INSERT INTO "sessions" (name, campaign_id, map_id)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: AddSessionAllowedCharacter :exec
+INSERT INTO "session_allowed_characters" (session_id, character_id)
+SELECT $1, unnest($2::bigint[])
+ON CONFLICT DO NOTHING;
